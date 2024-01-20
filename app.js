99ec5965f26b23c6e677e5abda501381d1bf2920
fiyaznkp <fiyaznkp@gmail.com>
2024-01-20 14:45:22 +0530

    excercises on js if statements

---
+++ app.js
@@ -0,0 +1,31 @@
+// const hour = 24
+
+// if(hour>6 && hour <12 ){
+//     console.log("good morning")
+// }else if(hour > 13 && hour <17){
+//     console.log("good evening")
+// }else{
+//     console.log("good night")
+// }
+
+// const age =70
+
+// const holiday = false
+
+// if(age < 6 || age >65 && holiday){
+//     console.log("you have special concern")
+// }
+// else{
+//     console.log("no concern")
+// }
+
+let result = ""
+const randonNumber = Math.random()
+
+if(randonNumber < 0.5 ){
+    result ="heads"
+}
+else{
+   result= "tails"
+}
+console.log(result)
