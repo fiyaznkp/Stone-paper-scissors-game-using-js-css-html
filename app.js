a79def03dfc1b69e019f55ced34186084e6dc222
fiyaznkp <fiyaznkp@gmail.com>
2024-01-21 13:23:25 +0530

    ternary operator activities

--- app.js
+++ app.js
@@ -38,15 +38,43 @@
 //     console.log("your guess is false")
 // }
 
-const randonNumber = Math.random()
+// const randonNumber = Math.random()
 
-let result =""
+// let result =""
 
-let guess ="tails"
+// let guess ="tails"
 
-result = randonNumber <= 0.5 ? "heads" :"tails"
+// result = randonNumber <= 0.5 ? "heads" :"tails"
 
-console.log(result)
+// console.log(result)
 
-console.log(guess === result ? "your guess is true": "your guess is false")
+// console.log(guess === result ? "your guess is true": "your guess is false")
+
+// const age = 5
+// const holiday = true    
+
+// const consideration = (age <6 || age > 65) && holiday ? "you have consideration":"no consideration"
+
+// console.log(consideration)
+
+// let score ="34"
+
+// let average =""
+
+// average = score < 30?"bad":(score >= 30 && score < 40? "good":"excellent")
+
+// console.log(average)
+
+let economy =19
+
+let performance = ""
+
+performance = economy <= 6 ? "excellent":
+             economy >6 && economy <= 7 ? "great":
+             economy > 7 && economy <= 8 ? "good":
+             economy > 8 && economy <= 9 ? "average":
+             economy > 9 && economy <= 10 ? "bad":"worst"
+             
+
+console.log(performance)
 
